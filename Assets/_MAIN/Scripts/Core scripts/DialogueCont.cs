71a7a07d0f4269198f524ce7880b60a69a0e4e78
pinkpuffle <francescajacques64@gmail.com>
2024-01-30 22:36:53 +0000

    dialogue container ini

--- Assets/_MAIN/Scripts/Core scripts/DialogueCont.cs
+++ Assets/_MAIN/Scripts/Core scripts/DialogueCont.cs
@@ -1,18 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class DialogueCont : MonoBehaviour
+public class DialogueCont
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public GameObject root;
+    public TMP_Text nameTxt;
+    public TMP_Text dialogueTxt;
 }
