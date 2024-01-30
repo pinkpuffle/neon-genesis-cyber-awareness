28513a9aa3b76839b4e159059e612fdd27ff7731
pinkpuffle <francescajacques64@gmail.com>
2024-01-30 23:14:31 +0000

    building

--- Assets/_MAIN/Scripts/Core scripts/TextArc.cs
+++ Assets/_MAIN/Scripts/Core scripts/TextArc.cs
@@ -4,6 +4,7 @@ using TMPro;
 
 public class TextArc
 {
+    //text types
     private TextMeshProUGUI tMProUI;
     private TextMeshPro tMProWorld;
     public TMP_Text tMPro => tMProUI != null ? tMProUI : tMProWorld;
@@ -15,11 +16,14 @@ public class TextArc
 
     public string fullTargetTxt => preTxt + targetTxt;
 
+    //select type
     public enum BuildMethod { instant, typewriter, fade}
     public BuildMethod buildMethod = BuildMethod.typewriter;
 
+    //control colour
     public Color txtColour { get { return tMPro.color; } set { tMPro.color = value; } }
 
+    //control speed
     public float speed { get { return baseSpeed * speedMultiplier; } set { speedMultiplier = value; } }
     private const float baseSpeed = 1;
     private float speedMultiplier = 1;
@@ -36,5 +40,47 @@ public class TextArc
     public TextArc(TextMeshPro tMProWorld)
     {
         this.tMProWorld = tMProWorld;
+    }
+
+    //build text
+    public Coroutine Build(string txt)
+    {
+        preTxt = "";
+        targetTxt = txt;
+
+        Stop();
+
+        buildProcess = tMPro.StartCoroutine(Building());
+        return buildProcess;
+    }
+
+    //append text to already built
+    public Coroutine Append(string txt)
+    {
+        preTxt = tMPro.text;
+        targetTxt = txt;
+
+        Stop();
+
+        buildProcess = tMPro.StartCoroutine(Building());
+        return buildProcess;
+    }
+
+
+    private Coroutine buildProcess = null;
+    public bool isBuilding => buildProcess != null;
+
+    public void Stop()
+    {
+        if (!isBuilding)
+            return;
+
+        tMPro.StopCoroutine(buildProcess);
+        buildProcess = null;
+    }
+
+    IEnumerator Building()
+    {
+        yield return null;
     }
 }
