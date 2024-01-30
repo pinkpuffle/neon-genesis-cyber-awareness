3ac0365e92b316682e4ec72fdd11eb6ff42158b3
pinkpuffle <francescajacques64@gmail.com>
2024-01-30 22:56:20 +0000

    txt config

--- Assets/_MAIN/Scripts/Core scripts/TextArc.cs
+++ Assets/_MAIN/Scripts/Core scripts/TextArc.cs
@@ -7,4 +7,20 @@ public class TextArc
     private TextMeshProUGUI tMProUI;
     private TextMeshPro tMProWorld;
     public TMP_Text tMPro => tMProUI != null ? tMProUI : tMProWorld;
+
+    public string currentTxt => tMPro.text;
+    public string targetTxt { get; private set; } = "";
+    public string preTxt { get; private set; } = "";
+    private int preTxtLength = 0;
+
+    public string fullTargetTxt => preTxt + targetTxt;
+
+    public enum BuildMethod { instant, typewriter, fade}
+    public BuildMethod buildMethod = BuildMethod.typewriter;
+
+    public Color txtColour { get { return tMPro.color; } set { tMPro.color = value; } }
+
+    public float speed { get { return baseSpeed * speedMultiplier; } set { speedMultiplier = value; } }
+    private const float baseSpeed = 1;
+    private float speedMultiplier = 1;
 }
