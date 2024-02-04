1cbddf37add4f724543330c5ef04ab1fdbd3a969
pinkpuffle <francescajacques64@gmail.com>
2024-02-04 22:26:53 +0000

    list and handling

--- Assets/_MAIN/Scripts/Core scripts/IO/FileManager.cs
+++ Assets/_MAIN/Scripts/Core scripts/IO/FileManager.cs
@@ -1,12 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class FileManager
 {
     public static List<string> ReadTxtFile(string filePath, bool includeBlankLines = true) //reference absolute path
     {
-        return null;
+        if (!filePath.StartsWith('/')) //if not absolute path
+            filePath = FilePaths.root + filePath;
+
+        List<string> lines = new List<string>();
+        try
+        {
+
+        }
+        catch(FileNotFoundException ex)
+        {
+            Debug.LogError($"File not found: '{ex.FileName}'");
+        }
+
+        return lunesl
     }
 
     public static List<string> ReadTxtAsset(string filePath, bool includeBlankLines = true) //reference within resources
