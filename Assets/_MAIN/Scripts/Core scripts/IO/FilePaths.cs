4f40f8b04fc71b5ef8cda6506e0bdacdfc2a3b20
pinkpuffle <francescajacques64@gmail.com>
2024-02-04 22:20:44 +0000

    add path

--- Assets/_MAIN/Scripts/Core scripts/IO/FilePaths.cs
+++ Assets/_MAIN/Scripts/Core scripts/IO/FilePaths.cs
@@ -1,18 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class FilePaths : MonoBehaviour
+public class FilePaths
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public static readonly string root = $"{Application.dataPath}/gameData/"; //path
 }
