3084f225a8bf2f8a46c2a076fe7611aadf9ef1db
pinkpuffle <francescajacques64@gmail.com>
2024-02-06 20:37:06 +0000

    test parsing

---
+++ Assets/_TESTING/Scripts/TestParsing.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace testing {
+    public class TestParsing : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            string line = "Speaker \"Dialogue goes in here\"";
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
