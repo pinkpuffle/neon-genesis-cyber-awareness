b7fc4b70e06ca15757dd900ad5e0d07059046dc1
pinkpuffle <francescajacques64@gmail.com>
2024-02-06 23:00:52 +0000

    find where dialogue

--- Assets/_MAIN/Scripts/Core scripts/Dialogue/DialogueParser.cs
+++ Assets/_MAIN/Scripts/Core scripts/Dialogue/DialogueParser.cs
@@ -15,9 +15,31 @@ namespace DIALOGUE
             return new DialogueLine(speaker, dialogue, commands);
         }
 
-        private static (string, string, string) RipContent(string rawLine)
+        private static (string, string, string) RipContent(string rawLine) //parses
         {
             string speaker = "", dialogue = "", commands = "";
+
+            int dialogueStart = -1;
+            int dialogueEnd = -1;
+            bool isEscaped = false;
+
+            for(int i = 0; i< rawLine.Length; i++ ) //find where dialogue
+            {
+                char current = rawLine[i]; //current char working on
+                if (current == '\\') //if \
+                    isEscaped = !isEscaped;
+                else if (current == '"' && !isEscaped) //start or end
+                {
+                    if (dialogueStart == -1)
+                        dialogueStart = i;
+                    else if (dialogueEnd == -1)
+                        dialogueEnd = i;
+                }
+                else
+                    isEscaped = false;
+            }
+
+
 
             return (speaker, dialogue, commands);
         }
