1dea821ee7a0557503f4c35cf9317df1b404cad8
pinkpuffle <francescajacques64@gmail.com>
2024-02-06 20:39:07 +0000

    output line

--- Assets/_MAIN/Scripts/Core scripts/Dialogue/DialogueParser.cs
+++ Assets/_MAIN/Scripts/Core scripts/Dialogue/DialogueParser.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 namespace DIALOGUE
 {
-
     public class DialogueParser
     {
-
+        public static DialogueLine Prase(string rawLine)
+        {
+            Debug.Log($"Parsing line: '{rawLine}'");
+            return null;
+        }
     }
 }
