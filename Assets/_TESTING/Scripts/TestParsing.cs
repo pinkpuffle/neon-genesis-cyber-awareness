fc93e047b2d8ad443028eb8a6843682f89c0ce87
pinkpuffle <francescajacques64@gmail.com>
2024-02-06 23:56:13 +0000

    parse file to console

--- Assets/_TESTING/Scripts/TestParsing.cs
+++ Assets/_TESTING/Scripts/TestParsing.cs
@@ -9,15 +9,23 @@ namespace testing {
         // Start is called before the first frame update
         void Start()
         {
-            string line = "Speaker \"Dialogue \\\"goes in\\\" here\" Command(arguments here)";
+            //string line = "Speaker \"Dialogue \\\"goes in\\\" here\" Command(arguments here)";
+            //DialogueParser.Parse(line);
 
-            DialogueParser.Prase(line);
+            SendFileToParse();
+
         }
 
-        // Update is called once per frame
-        void Update()
+        void SendFileToParse()
         {
+            List<string> lines = FileManager.ReadTxtAsset("testFile");
 
+            foreach(string line in lines)
+            {
+                if (line == string.Empty) //skip blank lines
+                    continue;
+                DialogueLine dl = DialogueParser.Parse(line); //parse each line, send to console
+            }
         }
     }
 }
