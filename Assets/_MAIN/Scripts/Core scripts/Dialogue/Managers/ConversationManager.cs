4ad06ba20f670685151cea046ee4a47b034e20e2
pinkpuffle <francescajacques64@gmail.com>
2024-02-12 01:43:13 +0000

    stop convo

--- Assets/_MAIN/Scripts/Core scripts/Dialogue/Managers/ConversationManager.cs
+++ Assets/_MAIN/Scripts/Core scripts/Dialogue/Managers/ConversationManager.cs
@@ -6,11 +6,24 @@ namespace DIALOGUE
 {
     public class ConversationManager
     {
+        private DialogueSys dialogueSys => DialogueSys.instance;
         private Coroutine process = null;
         public bool isRunning => process != null;
 
         public void StartConversation(List<string> conversation)
         {
+            StopConversation();
+
+            
+        }
+
+        public void StopConversation()
+        {
+            if (!isRunning)
+                return;
+
+            dialogueSys.StopCoroutine(process);
+            process = null;
 
         }
 
