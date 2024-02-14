a602aeafe2a5d01bc4e1b29be7052897c4c72c1a
pinkpuffle <francescajacques64@gmail.com>
2024-02-14 19:41:51 +0000

    lines from parsing
    same as it ever was

--- Assets/_TESTING/Scripts/TestDialogueFiles.cs
+++ Assets/_TESTING/Scripts/TestDialogueFiles.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DIALOGUE;
 
 public class TestConversation : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartConversation();
     }
 
-    // Update is called once per frame
-    void Update()
+    void StartConversation()
     {
-        
+        List<string> lines = FileManager.ReadTxtAsset("testFile");
+
+        foreach (string line in lines)
+        {
+            if (line == string.Empty) //skip blank lines
+                continue;
+            DialogueLine dl = DialogueParser.Parse(line); //parse each line, send to console
+        }
     }
+}
 }
