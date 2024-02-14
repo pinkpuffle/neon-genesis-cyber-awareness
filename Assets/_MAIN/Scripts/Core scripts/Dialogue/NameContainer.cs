185d95e197ec052a7af1a0c52e6c2fd43ce5d506
pinkpuffle <francescajacques64@gmail.com>
2024-02-14 20:06:29 +0000

    name container

---
+++ Assets/_MAIN/Scripts/Core scripts/Dialogue/NameContainer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///  box that holds name text on screen
+/// </summary>
+
+public class NameContainer : MonoBehaviour
+{
+    public void Show()
+    {
+
+    }
+
+    public void Hide()
+    {
+
+    }
+}
