28579590b24a0fe4c9b6a4cdab3335a1f149dcfc
pinkpuffle <francescajacques64@gmail.com>
2024-02-18 05:30:32 +0000

    comment out

--- Assets/_TESTING/Scripts/TestDialogueFiles.cs
+++ Assets/_TESTING/Scripts/TestDialogueFiles.cs
@@ -18,23 +18,31 @@ namespace testing {
         {
             List<string> lines = FileManager.ReadTxtAsset(fileToRead);
 
-            foreach(string line in lines)
-            {
-                if (string.IsNullOrEmpty(line))
-                    continue;
-
-                Debug.Log($"Segmenting line '{line}'");
-                DialogueLine dlLine = DialogueParser.Parse(line);
-
-                int i = 0;
-                foreach(DLDialogueData.DialogueSegment segment in dlLine.dialogue.segments)
-                {
-                    //print segment number, show what dialogue assigned, what signal assigned, if has delay show
-                    Debug.Log($"Segment [{i++}] = '{segment.dialogue}' [signal={segment.startSignal.ToString()}{(segment.signalDelay > 0 ? $" {segment.signalDelay}" : $"")}]");
-                }
-            }
-
             DialogueSys.instance.Say(lines);
+            
+            
+            
+            
+            
+            
+            
+            //foreach(string line in lines)
+            //{
+                //if (string.IsNullOrEmpty(line))
+                    //continue;
+
+                //Debug.Log($"Segmenting line '{line}'");
+                //DialogueLine dlLine = DialogueParser.Parse(line);
+
+                //int i = 0;
+                //foreach(DLDialogueData.DialogueSegment segment in dlLine.dialogue.segments)
+                //{
+                    //print segment number, show what dialogue assigned, what signal assigned, if has delay show
+                    //Debug.Log($"Segment [{i++}] = '{segment.dialogue}' [signal={segment.startSignal.ToString()}{(segment.signalDelay > 0 ? $" {segment.signalDelay}" : $"")}]");
+                //}
+            //}
+
+            
         }
     }
 }
