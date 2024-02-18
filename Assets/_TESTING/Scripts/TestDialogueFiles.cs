69dba1f8e7e5442e1f1ec82043507f60bf2b77c7
pinkpuffle <francescajacques64@gmail.com>
2024-02-18 05:14:14 +0000

    test some things

--- Assets/_TESTING/Scripts/TestDialogueFiles.cs
+++ Assets/_TESTING/Scripts/TestDialogueFiles.cs
@@ -18,6 +18,19 @@ namespace testing {
         {
             List<string> lines = FileManager.ReadTxtAsset(fileToRead);
 
+            foreach(string line in lines)
+            {
+                Debug.Log($"Segmenting line '{line}'");
+                DialogueLine dlLine = DialogueParser.Parse(line);
+
+                int i = 0;
+                foreach(DLDialogueData.DialogueSegment segment in dlLine.dialogue.segments)
+                {
+                    //print segment number, show what dialogue assigned, what signal assigned, if has delay show
+                    Debug.Log($"Segment [{i++}] = '{segment.dialogue}' [signal={segment.startSignal.ToString()}{(segment.signalDelay > 0 ? $" {segment.signalDelay}" : $"")}]");
+                }
+            }
+
             DialogueSys.instance.Say(lines);
         }
     }
