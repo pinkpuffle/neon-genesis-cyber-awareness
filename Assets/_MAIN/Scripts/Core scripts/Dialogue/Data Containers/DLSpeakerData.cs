a6aeb1591a39bd2a2d144fed8fd4e3db30abfdb7
pinkpuffle <francescajacques64@gmail.com>
2024-02-19 05:40:39 +0000

    if match for speaker name

--- Assets/_MAIN/Scripts/Core scripts/Dialogue/Data Containers/DLSpeakerData.cs
+++ Assets/_MAIN/Scripts/Core scripts/Dialogue/Data Containers/DLSpeakerData.cs
@@ -14,12 +14,21 @@ public class DLSpeakerData
         string pattern = @" as |  at | \[";
         MatchCollection matches = Regex.Matches(rawSpeaker, pattern);
 
-        if(matches.Count == 0) //no match - empty
+        //populate to avoid null values
+        castName = "";
+        castPosition = Vector2.zero;
+        CastExpressions = new List<(int layer, string expression)>();
+
+        if (matches.Count == 0) //no match - entire line speaker name
         {
             name = rawSpeaker;
-            castName = "";
-            castPosition = Vector2.zero;
-            CastExpressions = new List<(int layer, string expression)>();
+            return;
         }
+
+        //if match - isolate speaker name
+        int index = matches[0].Index;
+        name = rawSpeaker.Substring(0, index);
+
+
     }
 }
