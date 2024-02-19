ef5e4e9895aea5c770a172a52b34c1bb415978bf
pinkpuffle <francescajacques64@gmail.com>
2024-02-19 05:29:41 +0000

    speaker data

---
+++ Assets/_MAIN/Scripts/Core scripts/Dialogue/Data Containers/DLSpeakerData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DLSpeakerData
+{
+    public string name, castName;
+    public Vector2 castPosition;
+    public List<(int layer, string expression)> CastExpressions { get; set; }
+}
