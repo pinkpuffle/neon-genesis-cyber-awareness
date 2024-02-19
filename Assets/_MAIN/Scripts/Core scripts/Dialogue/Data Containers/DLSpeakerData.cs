0909cf9ce604100b4b5e788a534872a7a2d13726
pinkpuffle <francescajacques64@gmail.com>
2024-02-19 05:30:17 +0000

    constructor

--- Assets/_MAIN/Scripts/Core scripts/Dialogue/Data Containers/DLSpeakerData.cs
+++ Assets/_MAIN/Scripts/Core scripts/Dialogue/Data Containers/DLSpeakerData.cs
@@ -7,4 +7,9 @@ public class DLSpeakerData
     public string name, castName;
     public Vector2 castPosition;
     public List<(int layer, string expression)> CastExpressions { get; set; }
+
+    public DLSpeakerData(string rawSpeaker)
+    {
+
+    }
 }
