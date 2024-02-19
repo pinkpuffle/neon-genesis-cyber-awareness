c75587f695102be39c33d585c4bc4054d0b5d4e0
pinkpuffle <francescajacques64@gmail.com>
2024-02-19 06:27:48 +0000

    it works so remove

--- Assets/_TESTING/Scripts/TestDialogueFiles.cs
+++ Assets/_TESTING/Scripts/TestDialogueFiles.cs
@@ -18,54 +18,9 @@ namespace testing {
         {
             List<string> lines = FileManager.ReadTxtAsset(fileToRead);
 
-            for(int i = 0; i < lines.Count; i++) //print out everything
-            {
-                string line = lines[i];
 
-                if (string.IsNullOrWhiteSpace(line))
-                    continue;
+            DialogueSys.instance.Say(lines);
 
-                DialogueLine dl = DialogueParser.Parse(line);
-
-                Debug.Log($"{dl.speaker.name} as [{(dl.speaker.castName != string.Empty ? dl.speaker.castName : dl.speaker.name)}]at {dl.speaker.castPosition}");
-
-                List<(int l, string ex)> expr = dl.speaker.CastExpressions;
-                for(int c = 0; c < expr.Count; c++)
-                {
-                    Debug.Log($"[Layer[{expr[c].l}] = '{expr[c].ex}']");
-                }
-            }
-
-
-
-
-
-
-
-
-
-
-
-            //DialogueSys.instance.Say(lines);
-            
-            
-
-            
-            //foreach(string line in lines)
-            //{
-                //if (string.IsNullOrEmpty(line))
-                    //continue;
-
-                //Debug.Log($"Segmenting line '{line}'");
-                //DialogueLine dlLine = DialogueParser.Parse(line);
-
-                //int i = 0;
-                //foreach(DLDialogueData.DialogueSegment segment in dlLine.dialogue.segments)
-                //{
-                    //print segment number, show what dialogue assigned, what signal assigned, if has delay show
-                    //Debug.Log($"Segment [{i++}] = '{segment.dialogue}' [signal={segment.startSignal.ToString()}{(segment.signalDelay > 0 ? $" {segment.signalDelay}" : $"")}]");
-                //}
-            //}
 
             
         }
