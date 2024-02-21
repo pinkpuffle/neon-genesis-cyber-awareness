8c3e073d34ec39bc2ab876081b036862c4d9b949
pinkpuffle <francescajacques64@gmail.com>
2024-02-21 23:35:04 +0000

    for each command

--- Assets/_MAIN/Scripts/Core scripts/Dialogue/Data Containers/DLCommandData.cs
+++ Assets/_MAIN/Scripts/Core scripts/Dialogue/Data Containers/DLCommandData.cs
@@ -19,8 +19,17 @@ public class DLCommandData
         commands = RipCommands(rawCommands);
     }
 
-    private List<Command> RipCommands(string rawCammands)
+    private List<Command> RipCommands(string rawCommands)
     {
+        string[] data = rawCommands.Split(commandSplitterID, System.StringSplitOptions.RemoveEmptyEntries);
+        List<Command> result = new List<Command>();
 
+        foreach(string cmd in data)
+        {
+            Command command = new Command();
+            int index = cmd.IndexOf(argumentsContainerID); //index of parenthesis
+            command.name = cmd.Substring(0, index).Trim();
+            
+        }
     }
 }
