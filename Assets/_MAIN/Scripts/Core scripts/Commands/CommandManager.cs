e0a2f794d1a4f3de824adc7709b4b1ac3ee9c7cb
pinkpuffle <francescajacques64@gmail.com>
2024-02-22 00:40:46 +0000

    assign commands to database - extend

--- Assets/_MAIN/Scripts/Core scripts/Commands/CommandManager.cs
+++ Assets/_MAIN/Scripts/Core scripts/Commands/CommandManager.cs
@@ -20,6 +20,12 @@ public class CommandManager : MonoBehaviour
 
             Assembly assembly = Assembly.GetExecutingAssembly(); //reference to where code executing
             Type[] extensionTypes = assembly.GetTypes().Where(t => t.IsSubclassOf(typeof(CMDDatabaseExtension))).ToArray(); //every extension
+
+            foreach(Type extension in extensionTypes)
+            {
+                MethodInfo extendMethod = extension.GetMethod("Extend"); //get Extend method
+                extendMethod.Invoke(null, new object[] { database }); //null as object static, database passed in as object array
+            }
         }
         else
             DestroyImmediate(gameObject);
