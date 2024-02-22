12f675d96d08e215308ce306fe918b0218cdcb18
pinkpuffle <francescajacques64@gmail.com>
2024-02-22 00:32:47 +0000

    example command

---
+++ Assets/_MAIN/Scripts/Core scripts/Commands/Database/Extensions/CMDDatabaseExtension_Examples.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CMDDatabaseExtension_Examples : CMDDatabaseExtension
+{
+    new public static void Extend(CommandDatabase database)
+    {
+
+    }
+}
