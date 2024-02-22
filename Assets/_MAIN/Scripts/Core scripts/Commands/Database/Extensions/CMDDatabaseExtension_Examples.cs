cf0ec55820f06d9f73e0cd4351606b25281a3eb1
pinkpuffle <francescajacques64@gmail.com>
2024-02-22 00:46:41 +0000

    print line command

--- Assets/_MAIN/Scripts/Core scripts/Commands/Database/Extensions/CMDDatabaseExtension_Examples.cs
+++ Assets/_MAIN/Scripts/Core scripts/Commands/Database/Extensions/CMDDatabaseExtension_Examples.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class CMDDatabaseExtension_Examples : CMDDatabaseExtension
 {
     new public static void Extend(CommandDatabase database)
     {
+        //add command with no parameters
+        database.AddCommand("print", new Action(PrintDefaultMessage));
+    }
 
+    private static void PrintDefaultMessage()
+    {
+        Debug.Log("Printing a default message to console");
     }
 }
