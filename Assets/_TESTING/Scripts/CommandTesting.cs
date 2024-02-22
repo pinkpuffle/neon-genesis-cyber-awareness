695d92b7d55f468f5d9e25290d20150041a145b4
pinkpuffle <francescajacques64@gmail.com>
2024-02-22 00:55:23 +0000

    print command test

--- Assets/_TESTING/Scripts/CommandTesting.cs
+++ Assets/_TESTING/Scripts/CommandTesting.cs
@@ -7,7 +7,7 @@ public class CommandTesting : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        CommandManager.instance.Execute("print");
     }
 
     // Update is called once per frame
