631b2ddae44a5c156be1c57a9ae35552f583d6c9
pinkpuffle <francescajacques64@gmail.com>
2024-02-22 00:43:18 +0000

    add command

--- Assets/_MAIN/Scripts/Core scripts/Commands/Database/CommandDatabase.cs
+++ Assets/_MAIN/Scripts/Core scripts/Commands/Database/CommandDatabase.cs
@@ -8,4 +8,16 @@ public class CommandDatabase : MonoBehaviour
     private Dictionary<string, Delegate> database = new Dictionary<string, Delegate>();
 
     public bool HasCommand(string commandName) => database.ContainsKey(commandName);
+
+    public void AddCommand(string commandName, Delegate command)
+    {
+        if (!database.ContainsKey(commandName)) //if not already in
+        {
+            database.Add(commandName, command);
+        }
+        else
+        {
+            Debug.LogError($"Command already exists in database '{commandName}'");
+        }
+    }
 }
