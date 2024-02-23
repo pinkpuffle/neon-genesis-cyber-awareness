9dc143e4f54045beb897e41e77210337895ce65d
pinkpuffle <francescajacques64@gmail.com>
2024-02-23 03:32:33 +0000

    more data

--- Assets/_TESTING/Scripts/CommandTesting.cs
+++ Assets/_TESTING/Scripts/CommandTesting.cs
@@ -8,8 +8,8 @@ public class CommandTesting : MonoBehaviour
     void Start()
     {
         CommandManager.instance.Execute("print");
-        CommandManager.instance.Execute("print_1p");
-        CommandManager.instance.Execute("print_mp");
+        CommandManager.instance.Execute("print_1p", "Hello world!");
+        CommandManager.instance.Execute("print_mp", "Line1", "Line2", "Line3");
     }
 
     // Update is called once per frame
