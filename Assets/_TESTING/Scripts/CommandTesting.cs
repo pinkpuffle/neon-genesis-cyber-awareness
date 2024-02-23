1c0aafd5d88c4a055277934b8370470b7eefcd54
pinkpuffle <francescajacques64@gmail.com>
2024-02-23 04:23:54 +0000

    running format

--- Assets/_TESTING/Scripts/CommandTesting.cs
+++ Assets/_TESTING/Scripts/CommandTesting.cs
@@ -7,22 +7,21 @@ public class CommandTesting : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        CommandManager.instance.Execute("print");
-        CommandManager.instance.Execute("print_1p", "Hello world!");
-        CommandManager.instance.Execute("print_mp", "Line1", "Line2", "Line3");
-
-        CommandManager.instance.Execute("lambda");
-        CommandManager.instance.Execute("lambda_1p", "Hello lambda!");
-        CommandManager.instance.Execute("lambda_mp", "lambda1", "lambda2", "lambda3");
-
-        CommandManager.instance.Execute("process");
-        CommandManager.instance.Execute("process_1p", "3");
-        CommandManager.instance.Execute("process_mp", "Process line 1", "Process line 2", "Process line 3");
+        StartCoroutine(Running());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Running() //monitor for each end, only run next when
     {
-        
+        yield return CommandManager.instance.Execute("print");
+        yield return CommandManager.instance.Execute("print_1p", "Hello world!");
+        yield return CommandManager.instance.Execute("print_mp", "Line1", "Line2", "Line3");
+
+        yield return CommandManager.instance.Execute("lambda");
+        yield return CommandManager.instance.Execute("lambda_1p", "Hello lambda!");
+        yield return CommandManager.instance.Execute("lambda_mp", "lambda1", "lambda2", "lambda3");
+
+        yield return CommandManager.instance.Execute("process");
+        yield return CommandManager.instance.Execute("process_1p", "3");
+        yield return CommandManager.instance.Execute("process_mp", "Process line 1", "Process line 2", "Process line 3");
     }
 }
