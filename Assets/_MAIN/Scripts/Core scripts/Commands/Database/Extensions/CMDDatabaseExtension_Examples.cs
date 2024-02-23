6c0d188315fab8977ff21b5933598cd0d07cba7c
pinkpuffle <francescajacques64@gmail.com>
2024-02-23 03:29:38 +0000

    basic functions for printing

--- Assets/_MAIN/Scripts/Core scripts/Commands/Database/Extensions/CMDDatabaseExtension_Examples.cs
+++ Assets/_MAIN/Scripts/Core scripts/Commands/Database/Extensions/CMDDatabaseExtension_Examples.cs
@@ -9,10 +9,26 @@ public class CMDDatabaseExtension_Examples : CMDDatabaseExtension
     {
         //add command with no parameters
         database.AddCommand("print", new Action(PrintDefaultMessage));
+        database.AddCommand("print_1p", new Action<string>(printUserMessage)); //one param
+        database.AddCommand("print_mp", new Action<string[]>(PrintLines)); //multi param
     }
 
     private static void PrintDefaultMessage()
     {
         Debug.Log("Printing a default message to console");
+    }
+
+    private static void printUserMessage(string message)
+    {
+        Debug.Log($"User message: '{message}");
+    }
+
+    private static void PrintLines(string[] lines)
+    {
+        int i = 1;
+        foreach(string line in lines)
+        {
+            Debug.Log($"{i++}. '{line}'");
+        }
     }
 }
