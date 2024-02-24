26968275a20dce3cbad48106e5518c92c2940c7e
pinkpuffle <francescajacques64@gmail.com>
2024-02-24 15:44:58 +0000

    dialogue sys config

---
+++ Assets/_MAIN/Scripts/Core scripts/ScriptableObjects/DialogueSystemConfigurationSO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DIALOGUE
+{
+    public class DialogueSystemConfigurationSO : ScriptableObject
+    {
+
+    }
+}
