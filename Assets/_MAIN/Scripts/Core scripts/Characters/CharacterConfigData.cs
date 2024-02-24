3bbc5b9e0551d76bd075bc0b6812f25d5e82c05c
pinkpuffle <francescajacques64@gmail.com>
2024-02-24 15:18:30 +0000

    char data

---
+++ Assets/_MAIN/Scripts/Core scripts/Characters/CharacterConfigData.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace CHARACTERS
+{
+    public class CharacterConfigData
+    {
+        public string name;
+        public string alias;
+        public Character.CharacterType characterType;
+
+        public Color nameColour;
+        public Color dialogueColour;
+
+        public TMP_FontAsset nameFont;
+        public TMP_FontAsset dialogueFont;
+    }
+}
