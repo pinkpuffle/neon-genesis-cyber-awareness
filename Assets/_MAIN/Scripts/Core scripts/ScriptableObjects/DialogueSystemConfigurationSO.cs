fc1aff76099876d110fb0b6ab494ee02d8c92bc9
pinkpuffle <francescajacques64@gmail.com>
2024-02-24 15:50:54 +0000

    setup

--- Assets/_MAIN/Scripts/Core scripts/ScriptableObjects/DialogueSystemConfigurationSO.cs
+++ Assets/_MAIN/Scripts/Core scripts/ScriptableObjects/DialogueSystemConfigurationSO.cs
@@ -1,11 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using CHARACTERS;
 
 namespace DIALOGUE
 {
+    [CreateAssetMenu(fileName = "Dialogue System Configuration", menuName = "Dialogue System/Dialogue Configuration Asset")]
     public class DialogueSystemConfigurationSO : ScriptableObject
     {
-
+        public CharacterConfigSO characterConfigurationAsset;
     }
 }
