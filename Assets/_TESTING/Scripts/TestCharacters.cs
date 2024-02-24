2f9498c33866c35279ef7388602d63c44df8a2c7
pinkpuffle <francescajacques64@gmail.com>
2024-02-24 16:38:52 +0000

    test char

---
+++ Assets/_TESTING/Scripts/TestCharacters.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CHARACTERS;
+
+namespace TESTING
+{
+    public class TestCharacters : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Character character = CharacterManager.instance.CreateCharacter("Michael");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
