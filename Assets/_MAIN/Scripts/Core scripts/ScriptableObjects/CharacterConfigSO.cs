1bce3399f5cfbb2dc5b69c15e0b0dc3374d4b370
pinkpuffle <francescajacques64@gmail.com>
2024-02-24 15:12:47 +0000

    asset config

--- Assets/_MAIN/Scripts/Core scripts/ScriptableObjects/CharacterConfigSO.cs
+++ Assets/_MAIN/Scripts/Core scripts/ScriptableObjects/CharacterConfigSO.cs
@@ -2,17 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterConfigSO : MonoBehaviour
+namespace CHARACTERS
 {
-    // Start is called before the first frame update
-    void Start()
+    [CreateAssetMenu(fileName = "CharacterCOnfigurationAsset", menuName = "Dialogue System/Character Configuration Asset")]
+    public class CharacterConfigSO : ScriptableObject //as own asset
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
