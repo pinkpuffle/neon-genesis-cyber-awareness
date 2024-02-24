00a6cb38f6768cb0310bceb4e213adfe53756b53
pinkpuffle <francescajacques64@gmail.com>
2024-02-24 15:35:23 +0000

    character types

---
+++ Assets/_MAIN/Scripts/Core scripts/Characters/CharacterTypes/Character_Live2D.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CHARACTERS
+{
+    public class Character_Live2D : Character
+    {
+
+    }
+}
---
+++ Assets/_MAIN/Scripts/Core scripts/Characters/CharacterTypes/Character_Sprite.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CHARACTERS
+{
+    public class Character_Sprite : Character
+    {
+
+    }
+}
