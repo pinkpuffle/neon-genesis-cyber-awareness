720eb95d0a1deea1bf4847483b4f68d65cacbe9d
pinkpuffle <francescajacques64@gmail.com>
2024-03-06 02:35:01 +0000

    some lines for testing

--- Assets/_TESTING/Scripts/TestCharacters.cs
+++ Assets/_TESTING/Scripts/TestCharacters.cs
@@ -14,12 +14,46 @@ namespace TESTING
             //Character Michael = CharacterManager.instance.CreateCharacter("Michael");
             //Character Michael2 = CharacterManager.instance.CreateCharacter("Michael");
 
-            Character femaleStudent2 = CharacterManager.instance.CreateCharacter("Female Student 2");
+            //Character femaleStudent2 = CharacterManager.instance.CreateCharacter("Female Student 2");
+
+
+            StartCoroutine(Test());
+
         }
 
         // Update is called once per frame
         void Update()
         {
+
+        }
+
+        IEnumerator Test()
+        {
+            Character Nixon = CharacterManager.instance.CreateCharacter("Nixon");
+            Character Boss = CharacterManager.instance.CreateCharacter("Boss");
+            Character Hacker = CharacterManager.instance.CreateCharacter("Hacker");
+
+            List<string> lines = new List<string>()
+            {
+                "Hi there!",
+                "My name is Nixon",
+                "Oh, {wa 1} that's very nice"
+            };
+            
+            yield return Nixon.Say(lines);
+
+            lines = new List<string>()
+            {
+                "I am the Boss",
+                "More lines{c}Here."
+            };
+
+            yield return Boss.Say(lines);
+
+            yield return Hacker.Say("This is a line that I want to say.{a} It is a simple line.");
+
+            Debug.Log("Finished");
+
 
         }
     }
