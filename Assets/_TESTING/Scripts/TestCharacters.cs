398f5656518c433ec7a6752b8fcd6c7c016d4d1c
pinkpuffle <francescajacques64@gmail.com>
2024-03-06 05:31:59 +0000

    testing chars

--- Assets/_TESTING/Scripts/TestCharacters.cs
+++ Assets/_TESTING/Scripts/TestCharacters.cs
@@ -30,44 +30,44 @@ namespace TESTING
 
         IEnumerator Test()
         {
-            List<string> lines = new List<string>()
-            {
-                "narrator \"Hi\"",
-                "This is a line",
-                "And another",
-                "And a last one"
-            };
-            yield return DialogueSys.instance.Say(lines);
-
-            Debug.Log("Finished");
-
-
-
-
-            //Character Nixon = CharacterManager.instance.CreateCharacter("Nixon");
-            //Character Boss = CharacterManager.instance.CreateCharacter("Boss");
-            //Character Hacker = CharacterManager.instance.CreateCharacter("Hacker");
-
             //List<string> lines = new List<string>()
             //{
-            //    "Hi there!",
-            //    "My name is Nixon",
-            //    "Oh, {wa 1} that's very nice"
+            //    "narrator \"Hi\"",
+            //    "This is a line",
+            //    "And another",
+            //    "And a last one"
             //};
-
-            //yield return Nixon.Say(lines);
-
-            //lines = new List<string>()
-            //{
-            //    "I am the Boss",
-            //    "More lines{c}Here."
-            //};
-
-            //yield return Boss.Say(lines);
-
-            //yield return Hacker.Say("This is a line that I want to say.{a} It is a simple line.");
+            //yield return DialogueSys.instance.Say(lines);
 
             //Debug.Log("Finished");
+
+
+
+
+            Character Nixon = CharacterManager.instance.CreateCharacter("Nixon");
+            Character Boss = CharacterManager.instance.CreateCharacter("Boss");
+            Character Hacker = CharacterManager.instance.CreateCharacter("Hacker");
+
+            List<string> lines = new List<string>()
+            {
+                "Hi there!",
+                "My name is Nixon",
+                "Oh, {wa 1} that's very nice"
+            };
+
+            yield return Nixon.Say(lines);
+
+            lines = new List<string>()
+            {
+                "I am the Boss",
+                "More lines{c}Here."
+            };
+
+            yield return Boss.Say(lines);
+
+            yield return Hacker.Say("This is a line that I want to say.{a} It is a simple line.");
+
+            Debug.Log("Finished");
 
 
         }
