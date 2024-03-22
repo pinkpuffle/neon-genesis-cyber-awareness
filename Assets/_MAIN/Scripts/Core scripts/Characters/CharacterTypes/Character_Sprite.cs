2afddffbf11fd027a0f9fb3a2283242e0dfd392a
pinkpuffle <francescajacques64@gmail.com>
2024-03-22 22:05:26 +0000

    fading override

--- Assets/_MAIN/Scripts/Core scripts/Characters/CharacterTypes/Character_Sprite.cs
+++ Assets/_MAIN/Scripts/Core scripts/Characters/CharacterTypes/Character_Sprite.cs
@@ -6,9 +6,25 @@ namespace CHARACTERS
 {
     public class Character_Sprite : Character
     {
+        private CanvasGroup rootCG => root.GetComponent < CanvasGroup>();
         public Character_Sprite(string name, GameObject prefab, CharacterConfigData config) : base(name, prefab, config)
         {
             Debug.Log($"Created Sprite Character: '{name}'");
+        }
+
+        public override IEnumerator ShowingOrHiding(bool show)
+        {
+            float targetAlpha = show ? 1f : 0;
+            CanvasGroup self = rootCG;
+
+            while(self.alpha != targetAlpha)
+            {
+                self.alpha = Mathf.MoveTowards(self.alpha, targetAlpha, 3f * Time.deltaTime);
+                yield return null; //have reached target alpha
+            }
+
+            coRevealing = null;
+            coHiding = null;
         }
     }
 }
